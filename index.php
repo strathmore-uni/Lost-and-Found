97c2509cfcd1f88d59e8fc256146ad3289c9bf06
OvoFiona <146694437+OvoFiona@users.noreply.github.com>
2025-06-22 15:19:16 +0300

    index

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <title>Strathmore Lost & Found System</title>
     <link rel="stylesheet" href="CSS/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    
-      
+          
     </head>
 <body>
    <?php include_once("templates/nav.php"); ?> 
